a111b75720fa804c8169749861f6eb966d2f6c84
Hasan10100 <e20131@eng.pdn.ac.lk>
2026-02-06 03:07:45 +0530

    Implement electron playlist handlers

--- electron/preload.js
+++ electron/preload.js
@@ -15,5 +15,12 @@ contextBridge.exposeInMainWorld("api", {
         delete: (id) => ipcRenderer.invoke("videos:delete", id),
         upload: (payload) => ipcRenderer.invoke("videos:upload", payload),
         openExternal: (url) => ipcRenderer.invoke("videos:openExternal", url)
+    },
+    playlists: {
+        list: () => ipcRenderer.invoke("playlists:list"),
+        getlist: () => ipcRenderer.invoke("playlists:getlist", playlistId),
+        create: () => ipcRenderer.invoke("playlists:create", playlistName),
+        additem: () => ipcRenderer.invoke("playlists:addItem", playlistId, videoId),
+        removeitem: () => ipcRenderer.invoke("playlists:removeItem", playlistName, videoId),
     }
 });
